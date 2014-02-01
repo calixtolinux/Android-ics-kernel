630f289b6995c9b8266bab01f5f6d44b1b335bea
jithincalixto <jithin.s@calixto.co.in>
2014-02-01 11:04:13 +0530

    Add Support for Andriod ICS

---
+++ include/generated/compile.h
@@ -0,0 +1,6 @@
+/* This file is auto generated, version 44 */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#44 Fri Jan 31 12:03:27 IST 2014"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "shyam-desktop"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
